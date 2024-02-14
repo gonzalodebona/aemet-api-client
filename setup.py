4ff85a05c606fa5620f9115841be82e673c79901
Gonzalo de Bona <debonagonzalo@gmail.com>
2024-02-14 20:25:32 +0100

    Initial repository

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="aemet-api-client",
+    version="0.1.0",
+    description="Client for the Aemet API",
+    author="Gonzalo de Bona",
+    author_email="debonagonzalo@gmail.com",
+    packages=find_packages(),
+    classifiers=[
+            "Programming Language :: Python :: 3.9",
+        ]
+)
