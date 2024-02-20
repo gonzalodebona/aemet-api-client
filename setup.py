b6ac340655df61f5a6c2c1aa923b47ca058fe294
Gonzalo de Bona <debonagonzalo@gmail.com>
2024-02-20 19:16:27 +0100

    Upgrade version to 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="aemet-api-client",
-    version="0.1.0",
+    version="0.2.0",
     description="Client for the Aemet API",
     author="Gonzalo de Bona",
     author_email="debonagonzalo@gmail.com",
